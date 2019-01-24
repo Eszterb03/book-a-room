ab08403d66e328de478241da733c10f20b8ad2e0
davidjakoi <jakoidavid92@gmail.com>
2019-01-24 19:50:26 +0100

    feat(): started with authentication

--- server/calendar.js
+++ server/calendar.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const mongojs = require('mongojs');
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser');
 const db = mongojs('mongodb://Eszter:eszter2019@ds111455.mlab.com:11455/bookaroom', );
+
+const clientId = '297249123317-bhf732cn7r57eac3fk8bqp2976otj15u.apps.googleusercontent.com';
+const clientSecret = '7xk7sJbyF1MDASXmIhedjwxx';
