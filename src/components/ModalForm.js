61e15066070cbe2644a687ca2d2c3364bd5216e2
Milos Kallai <miloskallai@Miloss-Mac-mini.local>
2019-01-24 23:05:36 +0100

    (style) made modal component more responsive

--- src/components/ModalForm.js
+++ src/components/ModalForm.js
@@ -1,36 +1,49 @@
 import React from 'react';
 import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
-
-const Modal = ({ onChange, onClick, onSubmit, handleDayClick, selectedDay }) => {
-
+const Modal = ({
+  onChange,
+  onClick,
+  onSubmit,
+  handleDayClick,
+  selectedDay
+}) => {
   return (
     <div className='modal-form'>
       <div className='day-selector'>
-
         <DayPicker onDayClick={handleDayClick} />
         {selectedDay ? (
           <h2>{selectedDay.toLocaleDateString()}</h2>
         ) : (
-            <h2>Please select a day.</h2>
-          )}
+          <h2>Please select a day.</h2>
+        )}
       </div>
       <form onSubmit={onSubmit}>
-        <label>
-          Select room
-        <select id='room' name='room' onChange={onChange}>
-            <option value="Silent Room"></option>
-            <option value="Silent Room">Silent Room</option>
-            <option value="S Room">S Room</option>
-            <option value="M Room">M Room</option>
-            <option value="L Room">L Room</option>
-            <option value="Event Space">Event Space</option>
-          </select>
-        </label>
-        <label>
-          Select time between (8am - 9pm)
-        <input id='time' name='time' type='number' onChange={onChange} />
-        </label>
+        <label>Select room</label>
+        <select
+          className='form-input-field'
+          id='room'
+          name='room'
+          onChange={onChange}
+        >
+          <option value='Silent Room' />
+          <option value='Silent Room'>Silent Room</option>
+          <option value='S Room'>S Room</option>
+          <option value='M Room'>M Room</option>
+          <option value='L Room'>L Room</option>
+          <option value='Event Space'>Event Space</option>
+        </select>
+
+        <label>Select hour between 8am - 9pm (24h format)</label>
+        <input
+          className='form-input-field'
+          id='time'
+          name='time'
+          type='number'
+          onChange={onChange}
+        />
+
         <div className='btn-container'>
           <button className='button' type='submit'>
             OK
