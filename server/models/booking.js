cac9904f08ea5f4d0369f83b8aa5743766e60551
Mihori <gabor.dobrozemszky@hotmail.com>
2019-01-25 07:32:00 +0100

    fix(): remove user and allday

--- server/models/booking.js
+++ server/models/booking.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookingSchema = new Schema({
-    allday: { type: Boolean, required: true },
+    room: { type: String, required: true },
     start: { type: Date, required: true },
-    end: { type: Date, required: true },
-    user: { type: Schema.Types.ObjectId, ref: "users" },
-    title: { type: Schema.Types.ObjectId, ref: "rooms" }
+    end: { type: Date, required: true }
 });
 
 module.exports = mongoose.model('bookings', bookingSchema);
